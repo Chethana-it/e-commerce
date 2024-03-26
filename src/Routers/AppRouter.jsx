3c296a04e2074ed71978e39013583c231fe09abe
Chethana Jayasinghe <63340994+Chethana-it@users.noreply.github.com>
2024-03-26 10:52:23 +0530

    create inner routes with protected royter concept

--- src/Routers/AppRouter.jsx
+++ src/Routers/AppRouter.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainLayOut from '../components/layout/MainLayOut'
-
+import Home from '../pages/Home/home.jsx'
 const AppRouter = () => {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path='' element={<MainLayOut/>}>
-                  <Route index element={<Body/>}/>
+              <Route path='/' element={<MainLayOut/>}>
+                  <Route index element={<Home/>}/>
               </Route>
         </Routes>
       </BrowserRouter>
--- src/components/layout/MainLayOut.jsx
+++ src/components/layout/MainLayOut.jsx
@@ -7,7 +7,6 @@ const MainLayOut = () => {
   return (
     <div>
           <Header />
-          <Outlet/>
           <Footer/>
     </div>
   )
