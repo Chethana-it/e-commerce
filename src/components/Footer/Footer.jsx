e11f98bacecc70c955b2f953bc6f65510009299b
Chethana Jayasinghe <63340994+Chethana-it@users.noreply.github.com>
2024-04-07 17:02:03 +0530

    footer Icon fied and home padding

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,27 +8,28 @@ import  AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutline
 function Footer() {
   return (
     <footer className='fixed bottom-0 left-0 w-full z[100] p-1 bg-my-background flex items-center justify-between drop-shadow-header-shadow'>
-      <IconButton
-        sx={{
-          padding: '4px',
-          borderRadius: '5px'
-        }}
-      >
-        <HomeOutlinedIcon />
-        <p>Home</p>
-      </IconButton>
-
-      <IconButton>
-        <AppsOutlinedIcon/>
-        <p>Category</p>
-      </IconButton>
-
-      <IconButton>
-        < AccountCircleOutlinedIcon />
-        <p>Profile</p>
-      </IconButton>
+      <FooterIcon Icon={HomeOutlinedIcon} Name="Home" />
+      <FooterIcon Icon={AppsOutlinedIcon} Name="Category"/>
+      <FooterIcon Icon={AccountCircleOutlinedIcon} Name="Profile"/>
     </footer>
   )
 }
 
 export default Footer
+
+const FooterIcon = ({ Icon, Name }) => {
+  return (
+    <IconButton
+    sx={{
+      padding: '4px',
+      borderRadius: '5px',
+      backgroundColor: 'red'
+    }}
+    >
+      <div>
+        <Icon />
+        <p className='text-xs'>{ Name }</p>
+      </div>
+  </IconButton>
+  )
+}
