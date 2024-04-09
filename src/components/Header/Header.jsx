d4b076c7a87ae78a39e350f2b1dfe48ca0961167
Chethana Jayasinghe <63340994+Chethana-it@users.noreply.github.com>
2024-04-09 17:11:20 +0530

    mobile view search Icon display fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // import menu from '../../img/menu.png'
 // import search from '../../img/search.png'
@@ -10,6 +10,8 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { IconButton } from '@mui/material';
 
 function Header() {
+  console.log("Header Re-rendering...");
+  const [isSearch, setIsSearch] = useState(false)
   return (
     <header className='fixed top-0 left-0 w-full z-[100] p-1 bg-my-background flex items-center justify-between drop-shadow-header-shadow '>
       <div className='flex items-center'>
@@ -21,11 +23,43 @@ function Header() {
         }/>
         </IconButton>
         
-        <h1 className='text-sm font-semibold ml-5 col-auto font-white'>TECHOVIBE</h1>
+        <h1 className='text-sm font-semibold ml-5 col-auto text-yellow-50'
+        style={
+              {
+                display: window.innerWidth < 640
+                  ? isSearch
+                    ? 'none'
+                    : 'inline-block'
+                  : 'inline-block'
+              }
+            }
+        >TECHOVIBE</h1>
         <div className='overflow-hidden  outline-none p-2 font-semibold text-sm  ml-5 flex items-center '>
-          <input type='text' placeholder='Search' className='rounded-full  h-6 hidden sm:inline-block ' />
+          <input
+            type='text'
+            placeholder='Search'
+            
+            style={
+              {
+                display: window.innerWidth < 640
+                  ? isSearch
+                    ? 'inline-block'
+                    : 'none'
+                  : 'inline-block'
+              }
+            }
+            className='rounded-full  h-6 hidden sm:inline-block bg-slate-50 ' />
           {/* <img src={ search } alt='search' className='w-6 h-8 object-contain cursor-pointer ml-4 ' /> */}
-          <IconButton>
+          {/* <IconButton onClick={()=>{setIsSearch(true)}}> */}
+          <IconButton
+            sx={{
+              color:'white'
+            }}
+            onClick={() => {
+            if (window.innerWidth < 640) {
+              setIsSearch(!isSearch)
+            }
+          }}>
           <SearchIcon className='w-6 h-8 object-contain cursor-pointer ml-1 '/>
           </IconButton>
         </div>
@@ -35,7 +69,11 @@ function Header() {
         {/* left menu */}
         {/* <img src={cart } alt='cart' className='w-6 h-6 object-contain cursor-pointer'/>
          */}
-        <IconButton>
+        <IconButton
+        sx={{
+              color:'white'
+          }}
+        >
         <ShoppingCartIcon/>
         </IconButton>
         
